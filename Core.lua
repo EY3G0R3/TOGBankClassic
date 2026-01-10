024ac82ea54576c1b8ceac3f2db3c6d4febb1c08
Igor Gevka <igor.gevka@gmail.com>
2026-01-10 00:03:25 -0800

    Core.lua: block all outgoing comms while in a raid

--- Core.lua
+++ Core.lua
@@ -1,4 +1,15 @@
 TOGBankClassic_Core = LibStub("AceAddon-3.0"):NewAddon("TOGBankClassic", "AceComm-3.0", "AceConsole-3.0", "AceEvent-3.0", "AceSerializer-3.0", "AceTimer-3.0")
+local AceComm_SendCommMessage = TOGBankClassic_Core.SendCommMessage
+
+function TOGBankClassic_Core:SendCommMessage(prefix, text, distribution, target, prio, callbackFn, callbackArg)
+    if IsInRaid() then
+        return
+    end
+    if not AceComm_SendCommMessage then
+        return
+    end
+    return AceComm_SendCommMessage(self, prefix, text, distribution, target, prio, callbackFn, callbackArg)
+end
 
 function TOGBankClassic_Core:OnInitialize()
     -- Called when the addon is loaded
